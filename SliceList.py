514b128e3e062eff23446705d169f97aa90db89c
samrhatSunny <81028930+samrhatSunny@users.noreply.github.com>
2025-06-16 17:14:46 +0530

    Updated Code

--- SliceList.py
+++ SliceList.py
@@ -1,7 +1,11 @@
-
+n1 =[]
+n2 =[]
 def slice_num():
     lst_num = [1,2,3,4,5,6,7,8,9,10]
+    global n1,n2
     n1 = lst_num[:5:1]
     n2 = n1[::-1]
+
+def print_slice_num():
     print("Extracted List:",n1)
     print("Reverse List:",n2)
--- main.py
+++ main.py
@@ -7,14 +7,13 @@ import SliceList
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
+    #Task 1
     DicStudentMark.updateStudent()
     DicStudentMark.get_student()
+
+    #Task 2
     SliceList.slice_num()
+    SliceList.print_slice_num()
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
